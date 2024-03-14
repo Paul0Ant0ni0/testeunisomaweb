7eeffe67a562f413097ae31dd4e8b99e3ac3afe1
Paulo Antonio <dev.pauloantonio785@gmail.com>
2024-03-14 18:48:29 -0300

    Criado a aplicação

---
+++ src/main/java/com/teste/tecnicounisomaweb/TecnicounisomawebApplication.java
@@ -0,0 +1,13 @@
+package com.teste.tecnicounisomaweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecnicounisomawebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecnicounisomawebApplication.class, args);
+	}
+
+}
